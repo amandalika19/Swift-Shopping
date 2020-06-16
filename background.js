7e3035da3bc5cac2a8d3443a5f7733c74cff867d
Annapurna <amandalika19@outlook.com>
2020-06-16 13:25:11 -0700

    updated extension

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
 
-chrome.runtime.onInstalled.addListener(function()){
-    alert("Installed Extension! Welcome <3");
-}
+// chrome.runtime.onInstalled.addListener(function()){
+//     alert("Installed Extension! Welcome <3");
+// }
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
